8c8296e0c8a3c8d8ab6345a20f6364fe4834c384
碳黑橙子 <96968141+HChenZi@users.noreply.github.com>
2024-02-10 21:01:08 +0800

    114514 (#53)

---
+++ HChenZi/oom.js
@@ -0,0 +1,32 @@
+const array_1145141919810 = new Array();
+for (let i = 0; i < 11451400; i++) {
+  BCDEFGHIJKLMNO();
+}
+function BCDEFGHIJKLMNO () {
+  array_1145141919810.push("aaaaaaaaaaaaaaaaaaaa".repeat(23333));
+  array_1145141919810.push("bbbbbbbbbbbbbbbbbbbb".repeat(23333));
+  array_1145141919810.push("cccccccccccccccccccc".repeat(23333));
+  array_1145141919810.push("dddddddddddddddddddd".repeat(23333));
+  array_1145141919810.push("eeeeeeeeeeeeeeeeeeee".repeat(23333));
+  array_1145141919810.push("ffffffffffffffffffff".repeat(23333));
+  array_1145141919810.push("gggggggggggggggggggg".repeat(23333));
+  array_1145141919810.push("hhhhhhhhhhhhhhhhhhhh".repeat(23333));
+  array_1145141919810.push("iiiiiiiiiiiiiiiiiiii".repeat(23333));
+  array_1145141919810.push("jjjjjjjjjjjjjjjjjjjj".repeat(23333));
+  array_1145141919810.push("kkkkkkkkkkkkkkkkkkkk".repeat(23333));
+  array_1145141919810.push("llllllllllllllllllll".repeat(23333));
+  array_1145141919810.push("mmmmmmmmmmmmmmmmmmmm".repeat(23333));
+  array_1145141919810.push("nnnnnnnnnnnnnnnnnnnn".repeat(23333));
+  array_1145141919810.push("oooooooooooooooooooo".repeat(23333));
+  array_1145141919810.push("pppppppppppppppppppp".repeat(23333));
+  array_1145141919810.push("qqqqqqqqqqqqqqqqqqqq".repeat(23333));
+  array_1145141919810.push("rrrrrrrrrrrrrrrrrrrr".repeat(23333));
+  array_1145141919810.push("ssssssssssssssssssss".repeat(23333));
+  array_1145141919810.push("tttttttttttttttttttt".repeat(23333));
+  array_1145141919810.push("uuuuuuuuuuuuuuuuuuuu".repeat(23333));
+  array_1145141919810.push("vvvvvvvvvvvvvvvvvvvv".repeat(23333));
+  array_1145141919810.push("wwwwwwwwwwwwwwwwwwww".repeat(23333));
+  array_1145141919810.push("xxxxxxxxxxxxxxxxxxxx".repeat(23333));
+  array_1145141919810.push("yyyyyyyyyyyyyyyyyyyy".repeat(23333));
+  array_1145141919810.push("zzzzzzzzzzzzzzzzzzzz".repeat(23333));
+}
